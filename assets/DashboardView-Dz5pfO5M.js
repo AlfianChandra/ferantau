c479f42eed2a5a4c769a037e8bdcc8d9eb8a33cf
Alfian Chandra <31608738+AlfianChandra@users.noreply.github.com>
2025-01-17 09:10:27 +0700

    Update rantau
    fix auth-waiting

--- assets/DashboardView-Dz5pfO5M.js
+++
@@ -1,1 +0,0 @@
-import{_ as o}from"./MainWrapperView.vue_vue_type_style_index_0_lang-P6Cbb1s5.js";import{d as t,o as e,b as s}from"./index-PBH4u9Dq.js";import"./theme-DoyiLm4z.js";const m=t({__name:"DashboardView",setup(n){return(a,i)=>(e(),s(o,{"context-title":"Finding Oil Losses","context-desc":"https://rantau.findingoillosses.com"}))}});export{m as default};
