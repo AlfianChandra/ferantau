e7dd4ccb4cc2b5095832584052044e678f864116
Alfian Chandra <31608738+AlfianChandra@users.noreply.github.com>
2025-01-31 10:06:07 +0700

    -
    -

--- assets/blank-3eEf4x5c.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,r as t,o as a,c,a as n}from"./index-2ryfbf9o.js";const r={},s={class:"layout-wrapper layout-blank"};function _(l,p){const e=t("RouterView");return a(),c("div",s,[n(e)])}const u=o(r,[["render",_]]);export{u as default};
