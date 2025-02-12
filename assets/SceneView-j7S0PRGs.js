4840d8d7b69b89049d5c8b29b1155e44a20a1d66
Alfian Chandra <31608738+AlfianChandra@users.noreply.github.com>
2025-02-12 09:53:23 +0700

    Fix Model Window
    Fix model window error on closing the window

--- assets/SceneView-j7S0PRGs.js
+++ assets/SceneView-j7S0PRGs.js
@@ -1,4 +1,4 @@
-import{u as Ho}from"./theme-B1oxNx_L.js";import{_ as Vo}from"./MainWrapperView.vue_vue_type_style_index_0_lang-Bt6f2Btt.js";import{d as Go,h as ko,i as Wo,o as Xo,b as Yo,w as qo,e as Zo}from"./index-BngXA2lX.js";/**
+import{u as Ho}from"./theme-DlRanDZf.js";import{_ as Vo}from"./MainWrapperView.vue_vue_type_style_index_0_lang-F7ATbvIm.js";import{d as Go,h as ko,i as Wo,o as Xo,b as Yo,w as qo,e as Zo}from"./index-BZRzgBjQ.js";/**
  * @license
  * Copyright 2010-2025 Three.js Authors
  * SPDX-License-Identifier: MIT
